ff02a561df7b626ace17cc092fd2e87a7cb0b31f
Jon Clark <n6nqn6nq@gmail.com>
2017-09-13 06:38:18 -0700

    Other part of warning dialog.

---
+++ warninglistdialog.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import (QDialog, QMessageBox)
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QListWidgetItem
+import PyQt5.QtGui
+from warninglistdlg_auto import Ui_warninglistdlg
+
+class WarningListDialog(QDialog, Ui_warninglistdlg):
+    
+    def __init__(self, msg, affected_items):
+        super(WarningListDialog, self).__init__()
+        
+        if len(affected_items) == 0:
+            self.reply = True
+            return
+        self.setupUi(self)
+        self.reply = False
+        self.edtWarning.setPlainText(msg)
+        for l_item in affected_items:
+            self.listAffected.addItem(l_item)        
+
+        self.btnProceed.clicked.connect(lambda: self.proceed())
+        self.btnCancel.clicked.connect(lambda: self.cancel())
+        
+        self.exec_()
+        
+    def proceed(self):
+        self.reply = True
+        self.close()
+        return True
+    
+    def cancel(self):
+        self.reply = False
+        self.close()
+        return False
+    
