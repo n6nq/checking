670543f29a21cbf18478c675d2b7351ed6fc2efd
Jon Clark <n6nqn6nq@gmail.com>
2017-11-20 06:33:48 -0800

    Starting predictions.

--- predicted.py
+++ predicted.py
@@ -2,7 +2,9 @@
 will be used to project future balances for 'what-if scenarios.
 
 Amount
+Name
 category
+trigger
 type bill, prediction, subscription, monthly, elective
 cycle monthly, weekly, quarterly, annual, bi-weekly, ad-hoc
 date day-of-month, day-of-week, day/month, adhoc
@@ -10,5 +12,11 @@ date day-of-month, day-of-week, day/month, adhoc
 
 class Predicted(object):
     
-    def __init__(self):
+    def __init__(self, db, name, cat, trig=None, over=None, cat_id=0, trig_id=0, over_id=0, p_type=None, cycle=None, date=None, comment=None):
+        self.db = db
+        self.name = name
+        self.cat = cat
+        self.trig = trig
+        self.over = over
+        
         pass
