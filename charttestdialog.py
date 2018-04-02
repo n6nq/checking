b5340b870f2abe57d5e3159c7ba19f6da4b6a55d
Jon Clark <n6nqn6nq@gmail.com>
2018-04-02 06:24:48 -0700

    Adjusted graph scaling.

--- charttestdialog.py
+++ charttestdialog.py
@@ -1,6 +1,6 @@
 
 from PyQt5.QtWidgets import (QDialog, QFileDialog, QMenu, QAction, QListWidgetItem, QGraphicsScene, QInputDialog)
-from PyQt5.QtCore import (QLineF, QSize, QRectF)
+from PyQt5.QtCore import QLineF, QSize, QRectF, QPointF
 from database import Database
 import datetime
 from chart_test_auto import Ui_predictions
@@ -11,7 +11,9 @@ class ChartTestDialog(QDialog, Ui_predictions):
         super(ChartTestDialog, self).__init__()
         self.db = db
         self.setupUi(self)
-        
+
+        self.min_bal = 9999.99
+        self.max_bal = -9999.99
         today = datetime.date.today()
         str = 'Please import bank data up to today, {} and then enter the current balance.\nBalance:'.format(today.isoformat())
         values = QInputDialog.getText(self, 'Balance?', str)
@@ -21,8 +23,11 @@ class ChartTestDialog(QDialog, Ui_predictions):
         self.get_chart_data(today, values[0])
             
         self.scene = QGraphicsScene()
-        self.scene.setSceneRect(QRectF(0, 0, 1200, 800))
+        width = (self.nEntries+100) * 4
+        height = (self.max_bal - self.min_bal) / 100
+        self.scene.setSceneRect(QRectF(0, 0, width, height))
         self.graph.setScene(self.scene)
+        self.graph.centerOn(QPointF(width/2, height/2))
         self.graph.scale(1, -1)
         #self.resizeEvent.connect(self.resizeGraph())
         for i in range(0, self.nEntries-1):
@@ -30,8 +35,17 @@ class ChartTestDialog(QDialog, Ui_predictions):
         
         self.exec_()
     
-    def resizeEvent(self, evt):
+    def resizeEvent(self, evt) :
         self.resizeGraph(evt.size())
+ 
+#    def sceneScaleChanged(self, scale):
+#        scaleStr = self.sceneScaleCombo.currentText()
+#        scalestr = scaleStr[0 : scaleStr.index('%')]
+#        self.newScale = float(scalestr) / 100.0
+#        oldMatrix = self.view.transform()
+#        self.view.resetTransform()
+#        self.view.translate(oldMatrix.dx(), oldMatrix.dy());
+#        self.view.scale(self.newScale, self.newScale)
         
     def resizeGraph(self, size):
         width = size.width()
@@ -51,5 +65,8 @@ class ChartTestDialog(QDialog, Ui_predictions):
         for ent in reversed_entries:
             running -= ent.amount.value
             self.balances.append(running)
+            
         self.balances.reverse()
+        self.max_bal = max(self.balances)
+        self.min_bal = min(self.balances)
         
